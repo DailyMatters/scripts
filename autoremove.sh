f34a587b7967b0cd500b818c1a2e2a5117e7bb7c
Claudio Ribeiro <cribeiro@ventureoak.com>
2017-11-02 12:57:57 +0000

    beautifier

--- autoremove.sh
+++ autoremove.sh
@@ -5,5 +5,5 @@ apt-get autoremove -y
 " "Provides full partition availability
 df -h | awk 'NR!=1{print $1, $4, $5}' >> /tmp/availability.txt
 
-" "Alerts for partitions getting too big
+" "Alerts partitions getting too big
 df -h | awk 'BEGIN{system("date '+%Y/%m/%d'");}$5 > 75 && NR != 1 {print "Alert!!! Partition",$6, "is", $5, "full."}'
