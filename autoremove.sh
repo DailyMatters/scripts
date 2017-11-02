fa84a7899a3c842f8b7a7f00817501b80c5d3c80
Claudio Ribeiro <cribeiro@ventureoak.com>
2017-11-02 12:44:54 +0000

    Added date to verification

--- autoremove.sh
+++ autoremove.sh
@@ -6,4 +6,4 @@ apt-get autoremove -y
 df -h | awk 'NR!=1{print $1, $4, $5}' >> /tmp/availability.txt
 
 " "Alerts for partitions getting too big
-df -h | awk '$5 > 85 && NR != 1 {print "Alert!!! Partition",$6, "is", $5, "full."}' >> /home/claudio/Desktop/alert.txt
+df -h | awk 'BEGIN{system("date '+%Y/%m/%d'");}$5 > 75 && NR != 1 {print "Alert!!! Partition",$6, "is", $5, "full."}'
